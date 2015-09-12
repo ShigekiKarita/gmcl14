c8540d6cde6d4c3a9a28a93cc02bc6b5a4ec413b
karita <shigekikarita@gmail.com>
2015-09-12 22:55:51 +0900

    add symm

--- src/gmcl14/formats/symmetry.hpp
+++ src/gmcl14/formats/symmetry.hpp
@@ -6,6 +6,26 @@ namespace MatrixICCL
     class Symmetry : public Format
     {
     public:
-        using config = 
+        using config       = typename Format::config;
+        using index_type   = typename config::index_type;
+        using element_type = typename config::element_type;
+
+        Symmetry(const index_type& rows, const index_type& cols)
+            : Format(rows, cols)
+        {}
+
+        auto& set_element(const index_type& i, const index_type& j, const element_type& v)
+        {
+            return i >= j
+                ? Format::set_element(i, j, v)
+                : Format::set_element(j, i, v);
+        }
+
+        const auto& get_element(const index_type& i, const index_type& j)
+        {
+            return i >= j
+                ? Format::get_element(i, j)
+                : Format::get_element(j, i);
+        }
     };
 }
